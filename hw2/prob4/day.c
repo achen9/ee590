f3cdf15499594e9531061e4f6ace29cfdbbb0d17
Alex Chen <alexac9@u.washington.edu>
2016-10-09 18:01:25 -0700

    Update /hw2/prob4/day.c data types

--- hw2/prob4/day.c
+++ hw2/prob4/day.c
@@ -33,38 +33,38 @@
 #include <stdlib.h>
 #include <limits.h>
 
-static char daytab[2][13] = {
+static char *daytab[] = {
   { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
   { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
 };
 
-int day_of_year(int year, int month, int day);
-void month_day(int year, int yearday, int *pmonth, int *pday);
+long day_of_year(long year, long month, long day);
+void month_day(long year, long yearday, long *pmonth, long *pday);
 
-int day_of_year(int year, int month, int day)
+long day_of_year(long year, long month, long day)
 {
   printf("day_of_year() not implemented yet...\n");
   return 0;
 }
-void month_day(int year, int yearday, int *pmonth, int *pday)
+void month_day(long year, long yearday, long *pmonth, long *pday)
 {
   int i, leap;
 
   leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-  for (i = 1; yearday > *(daytab + leap * 13 + i), i++) {
-    yearday -= *(daytab + leap * 13 + i);
+  for (i = 1; yearday > *(*(daytab + leap) + i); i++) {
+    yearday -= *(*(daytab + leap) + i);
   }
   *pmonth = i;
   *pday = yearday;
 }
 int main(int argc, char *argv[])
 {
-  long year, month, day, yearday;
-  int *pmonth, *pday;
+  long year = 0, month = 0, day = 0, yearday = 0;
+  char *endp;
 
-  year = strtol(argv[1], NULL, 10);
-  if (INT_MAX < year || INT_MIN > year) {
-    printf("Too large of a year entered.\nEnter a year between %d and %d\n", INT_MIN, INT_MAX);
+  year = strtol(argv[1], &endp, 10);
+  if ('\0' != *endp) {
+    printf("Invalid year entered.\nEnter a year between %ld and %ld", LONG_MIN, LONG_MAX);
   }
   if (3 == argc) {
     yearday = strtol(argv[2], NULL, 10);
@@ -72,8 +72,8 @@ int main(int argc, char *argv[])
       printf("Invalid yearday entered.\nEnter a yearday between 1 and 366.\n");
       exit(-2);
     }
-    month_day((int)year, (int)yearday, pmonth, pday);
-    printf("%d %d", *pmonth, *pday);
+    month_day(year, yearday, &month, &day);
+    printf("%ld %ld", *pmonth, *pday);
   } else if (4 == argc) {
     month = strtol(argv[2], NULL, 10);
     if (1 > month || 12 < month) {
@@ -85,8 +85,8 @@ int main(int argc, char *argv[])
       printf("Invalid day entered.\nEnter a day between 1 and 31.\n");
       exit(-2);
     }
-    yearday = day_of_year((int)year, (int)month, (int)day);
-    printf("%d", yearday);
+    yearday = day_of_year(year, month, day);
+    printf("%ld", yearday);
   } else {
     printf("Incorrect number of inputs entered.\n");
     return -1;

bc8a42197d746cbe268c9369e2e27c247cf187db
Alex Chen <alexac9@u.washington.edu>
2016-10-10 17:43:01 -0700

    Update /hw2/prob5/example.c to test matrix_equal()

--- hw2/prob5/example.c
+++ hw2/prob5/example.c
@@ -28,25 +28,23 @@ int main ( int argc, char * argv[] ) {
   Matrix * A = matrix_new ( 2, 2 ),
          * B = matrix_random ( 4, 4 ),
          * C;
+  int check;
 
   matrix_set ( A, 0, 0, 1 ); matrix_set ( A, 0, 1, 2 );
   matrix_set ( A, 1, 0, 3 ); matrix_set ( A, 1, 1, 4 );
 
+  printf("Beginning matrix_equal() test...\n");
   C = matrix_add ( A, A );
-  matrix_print ( C ); NL;
-  matrix_destroy ( C );
+  check = matrix_equal(C, A);
+  printf("C == A? %d\n", check);
+  check = matrix_equal(B, A);
+  printf("B == A? %d\n", check);
+  check = matrix_equal(A, A);
+  printf("A == A? %d\n\n", check);
 
-  matrix_print ( B ); NL;
-  C = matrix_minor ( B, 1 );
-  matrix_print ( C ); NL;
-  matrix_destroy ( C );
 
-  printf ( "det = %lf\n", matrix_det ( B ) );
-
-  C = matrix_from_file ( "data/m1.mat" );
-  matrix_print(C); NL;
-  printf ( "det = %lf\n", matrix_det ( C ) );
-
+  matrix_destroy(A);
+  matrix_destroy(B);
   matrix_destroy(C);
 
   return 0;

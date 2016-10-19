017bd4599dc6ffe9c9ac7fb751b08caf6f7ea03e
Alex Chen <alexac9@u.washington.edu>
2016-10-19 16:01:15 -0700

    Update hw3/prob1/test/5.cc

--- hw3/prob1/test/5.cc
+++ hw3/prob1/test/5.cc
@@ -13,25 +13,31 @@ int main ( int argc, char * argv[] ) {
   fraction x = x1 + x2;
   ASSERT(5 == x.num() && 6 == x.den());
 
-  // TEST 2: Subtract : 2/4 - 2/3 = -1/6
+  // TEST 2: Add : (1/2 + 2/-4) + 5/6 = 5/6
+  fraction s1(1, 2);
+  fraction s2(2, -4);
+  fraction s = (s1 + s2) + x;
+  ASSERT(5 == s.num() && 6 == s.den());
+
+  // TEST 3: Subtract : 2/4 - 2/3 = -1/6
   fraction y1(2, 4);
   fraction y2(2, 3);
   fraction y = y1 - y2;
   ASSERT(-1 == y.num() && 6 == y.den());
 
-  // TEST 3: Multiply : -4/5 * 3/4 = -3/5
+  // TEST 4: Multiply : -4/5 * 3/4 = -3/5
   fraction w1(-4, 5);
   fraction w2(3, 4);
   fraction w = w1 * w2;
   ASSERT(-3 == w.num() && 5 == w.den());
 
-  // TEST 4: Divide : (-5/7) / (-6/7) = 5/6
+  // TEST 5: Divide : (-5/7) / (-6/7) = 5/6
   fraction z1(-5, 7);
   fraction z2(-6, 7);
   fraction z = z1 / z2;
   ASSERT(5 == z.num() && 6 == z.den());
   
-  // TEST 5: Divide by zero : (2/3) / (0/2) = error
+  // TEST 6: Divide by zero : (2/3) / (0/2) = error
   fraction r1(2, 3);
   fraction r2(0, 2);
   try {

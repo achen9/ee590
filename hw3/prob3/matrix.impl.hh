ed7bb89bf09baf8327623a2f7917d4c843a4b46e
Alex Chen <alexac9@u.washington.edu>
2016-10-22 14:06:16 -0700

    Add hw3/prob3/lec3b code

---
+++ hw3/prob3/matrix.impl.hh
@@ -0,0 +1,120 @@
+
+template <class T>
+matrix<T>::matrix ( int r, int c ) : num_rows(r), num_columns(c) {
+
+  if ( r <= 0 || c <= 0 ) {
+    throw matrix_exception("Attempted to initialize a matrix with non-positive number of rows and/or columns");
+  }
+
+  value = new T[r*c];
+
+  for ( int i=0; i<r*c; i++ ) {
+    value[i] = (T) 0;
+  }
+
+}
+
+template <class T>
+matrix<T>::~matrix() {
+  delete []value;
+}
+/*
+template <class T>
+matrix<T> matrix<T>::random(int r, int c, T min, T max) {
+
+  if (r <= 0 || c <= 0) {
+    throw matrix_exception("Attempted to build a random matrix with non-positive number of rows and/or columns");
+  }
+
+  if (max <= min) {
+    throw matrix_exception("Min is not less than max when building a random matrix");
+  }
+
+  matrix m(r, c);
+  T d = max - min + 1,
+    h = d / 2;
+
+  for (int i = 0; i<r; i++) {
+    for (int j = 0; j<c; j++) {
+      m.set(i, j, rand() % d - h);
+    }
+  }
+
+  return m;
+
+}
+*/
+template <class T>
+matrix<T> &matrix<T>::operator=(const matrix &other) {
+
+  if (this != &other) {
+    num_rows = other.num_rows;
+    num_columns = other.num_columns;
+    T * temp = new T[num_rows*num_columns];
+    for (int i = 0; i<num_rows*num_columns; i++) {
+      temp[i] = other.value[i];
+    }
+    delete[]value;
+    value = temp;
+  }
+
+  return *this;
+
+}
+
+template <class T>
+matrix<T> matrix<T>::add ( const matrix &m ) const {
+
+  if ( rows() != m.rows() || columns() != m.columns() ) {
+    throw matrix_exception("Attemped to add matrices with incompatible sizes");
+  }
+
+  matrix M(rows(),columns());
+
+  for ( int i=0; i<rows(); i++ ) {
+    for ( int j=0; j<columns(); j++ ) {
+      M.set(i,j,(T) (get(i,j) + m.get(i,j))); /* NOTE: Casting result back to T */
+    }
+  }
+
+  return M;
+
+}
+
+template <class T>
+T matrix<T>::get(int r, int c) const {
+
+  if ( !in_range(r,c) ) {
+    throw matrix_exception("Indices out of range in matrix::get");
+  }
+
+  return value[r * columns() + c];
+
+}
+
+template <class T>
+void matrix<T>::set(int r, int c, T x) {
+  if ( !in_range(r,c) ) {
+    throw matrix_exception("Indices out of range in matrix::set");
+  }
+  value[r * columns() + c] = x;
+}
+
+template <class T>
+bool matrix<T>::in_range(int r, int c) const {
+  return 0 <= r && r < rows() && 0 <= c && c < columns();
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const matrix<T> &m) {
+
+  for ( int i=0; i<m.rows(); i++ ) {
+    for ( int j=0; j<m.columns(); j++ ) {
+      os << m.get(i,j) << '\t';
+    }
+    os << std::endl;
+  }
+
+  return os;
+
+}

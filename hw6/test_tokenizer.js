7675ce61dcf0a8d0a7dc5e6cbfe3c0114187bf9c
Alex Chen <alexac9@u.washington.edu>
2016-11-13 18:45:36 -0800

    Update hw6/test_tokenizer.js with current() and eat() tests

--- hw6/test_tokenizer.js
+++ hw6/test_tokenizer.js
@@ -1,7 +1,8 @@
+// TOKENIZER FUNCTION TESTS
+// This file tests the Tokenizer functions 
 Tokenizer = require('./tokenizer.js');
-
+assert = require('assert');
 t = new Tokenizer();
-
 test_string = "-12. + 0.5 * (-1   + 2 / 3.0e+2 / -.9)"
 
 t.add(/((-|\+)?)\d*.\d*(e|E)((-|\+)?)\d+/) // Parse scientific notation
@@ -11,18 +12,13 @@ t.add(/((-|\+)?)\d*.\d*(e|E)((-|\+)?)\d+/) // Parse scientific notation
  .add(/\s*/)                               // Parse whitespaces
  .tokenize(test_string);
 
-console.log(t.tokens.length);
-console.log(t.tokens);
+// Test 1: Test current() function
+assert(-12 == parseFloat(t.current()));
 
+// Test 2: Test eat() function
+t.eat();
+t.eat();
+assert('+' == t.current());
 
-// Test 1: current() function
-console.log(t.current());
-
-// Test 2: eat() function
-/*for (var i = 0; i < t.tokens.length; i++) {
-  console.log(t.eat());
-}
-console.log(t.eat());
-console.log(t.current());
-*/
 // Test 3: eof() function
+

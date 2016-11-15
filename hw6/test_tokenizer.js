62368da8f24ff442a44ceb5156f4274bc038de44
Alex Chen <alexac9@u.washington.edu>
2016-11-14 18:02:55 -0800

    Update hw6/test_tokenizer.js to not recognize negative numbers

--- hw6/test_tokenizer.js
+++ hw6/test_tokenizer.js
@@ -5,17 +5,18 @@ assert = require('assert');
 t = new Tokenizer();
 test_string = "-12. + 0.5 * (-1   + 2 / 3.0e+2 / -.9)"
 
-t.add(/((-|\+)?)\d*.\d*(e|E)((-|\+)?)\d+/) // Parse scientific notation
- .add(/(-?)\d*\.\d*/)                      // Parse  floating points
- .add(/-?\d+/)                             // Parse integers
- .add(/\+|-|\*|\/|%|\(|\)/)                // Parse arithmetic operators and parenthesis
- .add(/\s*/)                               // Parse whitespaces
+t.add(/\d*.\d*(e|E)((-|\+)?)\d+/) // Parse scientific notation
+ .add(/\d*\.\d*/)                 // Parse  floating points
+ .add(/\d+/)                      // Parse integers
+ .add(/\+|-|\*|\/|%|\(|\)/)       // Parse arithmetic operators and parenthesis
+ .add(/\s*/)                      // Parse whitespaces
  .tokenize(test_string);
 
 // Test 1: Test current() function
-assert(-12 == parseFloat(t.current()));
+assert('-' == t.current());
 
 // Test 2: Test eat() function
+t.eat();
 t.eat();
 assert('+' == t.eat());
 

ce65130666b27a8d3eac66077660cd165d19c124
Alex Chen <alexac9@u.washington.edu>
2016-11-14 18:24:43 -0800

    Update hw6/parser.js to interpret '-' and '+' signs

--- hw6/parser.js
+++ hw6/parser.js
@@ -5,19 +5,21 @@ function Parser(str) {
   this.str = str; // the string to be parsed
   this.tokenizer = new Tokenizer();
   this.tokenizer
-   .add(/((-|\+)?)\d*.\d*(e|E)((-|\+)?)\d+/) // Parse scientific notation
-   .add(/(-?)\d*\.\d*/)                      // Parse  floating points
-   .add(/-?\d+/)                             // Parse integers
-   .add(/\+|-|\*|\/|%|\(|\)/)                // Parse arithmetic operators and parenthesis
-   .add(/\s*/)                               // Parse whitespaces
+   .add(/\d*.\d*(e|E)((-|\+)?)\d+/) // Parse scientific notation
+   .add(/\d*\.\d*/)                 // Parse  floating points
+   .add(/\d+/)                      // Parse integers
+   .add(/\+|-|\*|\/|%|\(|\)/)       // Parse arithmetic operators and parenthesis
+   .add(/\s*/)                      // Parse whitespaces
   this.tokenizer.tokenize(str);
 }
 
 Parser.prototype.factor = function () {
   this.tokenizer.eat_whitespace();
   var pf = this.tokenizer.float_val();
-  if(!isNaN(pf)) {
-    this.tokenizer.eat();
+  if (!isNaN(pf)) {
+    if (!this.tokenizer.eof()) {
+      this.tokenizer.eat();
+    }
     return pf;
   } else if('(' == this.tokenizer.current()) {
     this.tokenizer.eat();
@@ -25,23 +27,39 @@ Parser.prototype.factor = function () {
     pf = this.expr();
     this.tokenizer.eat_whitespace();
     if (')' == this.tokenizer.current()) {
-      this.tokenizer.eat();
+      if (!this.tokenizer.eof()) {
+        this.tokenizer.eat();
+      }
     } else {
-      throw new ParserException("Incorrect token " + this.tokenizer.current(), this.tokenizer.current_token_pos);
-    }    
+      var pos = 0;
+      for (var i = 0; i < this.tokenizer.current_token_pos; i++) {
+        pos += this.tokenizer.tokens[i].length;
+      }
+      throw new ParserException("Unexpected token '" + this.tokenizer.current() + "'", pos + 1);
+    }
     return pf;
+  } else if ('-' == this.tokenizer.current() | '+' == this.tokenizer.current()) {
+    pf = this.expr();
+    return pf;
+  } else {
+    var pos = 0;
+    for (var i = 0; i < this.tokenizer.current_token_pos; i++) {
+      pos += this.tokenizer.tokens[i].length;
+    }
+    throw new ParserException("Unexpected token '" + this.tokenizer.current() + "'", pos + 1);
   }
 }
 
-Parser.prototype.term = function() {
+Parser.prototype.term = function(sign) {
   var f;
   var number;
   this.tokenizer.eat_whitespace();
-  number = this.factor();
-  debugger;
+  number = this.factor() * sign;
   this.tokenizer.eat_whitespace();
-  while('*' == this.tokenizer.current() | '/' == this.tokenizer.current() | '%' == this.tokenizer.current()) {
-    var operator = this.tokenizer.eat();
+  
+  while ('*' == this.tokenizer.current() | '/' == this.tokenizer.current() | '%' == this.tokenizer.current()) {
+    var operator = this.tokenizer.current();
+    this.tokenizer.eat();
     this.tokenizer.eat_whitespace();
     f = this.factor();
     this.tokenizer.eat_whitespace();
@@ -59,19 +77,28 @@ Parser.prototype.term = function() {
 Parser.prototype.expr = function() {
   var t;
   var number;
+  var sign = 1;
   this.tokenizer.eat_whitespace();
-  number = this.term();
+  while ('-' == this.tokenizer.current() | '+' == this.tokenizer.current()) {
+    var op = this.tokenizer.current();
+    this.tokenizer.eat();
+    this.tokenizer.eat_whitespace();
+    if ('-' == op) {
+      sign *= -1;
+    }
+  }
+  number = this.term(sign);
   this.tokenizer.eat_whitespace();
   while('-' == this.tokenizer.current() | '+' == this.tokenizer.current()) {
-    var operator = this.tokenizer.eat();
-    this.tokenizer.eat_whitespace();
-    t = this.term();
+    var operator = this.tokenizer.current();
+    this.tokenizer.eat();
     this.tokenizer.eat_whitespace();
     if('-' == operator) {
-      number -= t;
+      t = this.term(-1);
     } else {
-      number += t;
+      t = this.term(1);
     }
+    number += t;
   }
   return number;
 }

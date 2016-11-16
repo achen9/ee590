e8166d36ee6a5c2093adf23139d963498ec2c5b0
Alex Chen <alexac9@u.washington.edu>
2016-11-15 19:43:25 -0800

    Update hw6/test unit tests

--- hw6/test/parser_unit_tests.js
+++ hw6/test/parser_unit_tests.js
@@ -6,20 +6,60 @@ assert = require('assert');
 // Test 1: Test factor() function
 p1 = new Parser("12e3");
 assert(12e3 == p1.factor(0));
-console.log("Passed test 1");
 
 // Test 2: Test term() function
 p2 = new Parser("5 % 4 * 2.5 * 4 /2");
 assert(5 == p2.term(0, 1));
-console.log("Passed test 2");
 
 // Test 3: Test expr() function
 p3 = new Parser("2.4-5+4");
-debugger;
 assert(1.4 == p3.expr(0));
-console.log("Passed test 3");
 
 // Test 4: Test all functions in parser together
-test_str = "-12. + 0.5 * (-1   + 2 / 3.0e+2 / -.9)";
+test_str = "-12. + 0.5 * (-1 -+-+ 2 / 3.0e+2 / -.9)";
 p = new Parser(test_str);
 console.log(test_str + " evaluates to " + p.parse());
+
+// Test 5: Test ParserException() function
+try {
+  p4 = new Parser("((12%5)");
+  console.log('Trying to parse "((12%5)"');
+  p4.parse();
+} catch(e) {
+  console.log(e.name + ": " + e.msg + " at position " + e.position);
+}
+try {
+  p5 = new Parser(".3e+2*.2)");
+  console.log('Trying to parse ".3e+2*.2)"');
+  p5.parse();
+} catch(e) {
+  console.log(e.name + ": " + e.msg + " at position " + e.position);
+}
+try {
+  p6 = new Parser("13.5(12.6 + 6)");
+  console.log('Trying to parse "13.5(12.6 + 6)"');
+  p6.parse();
+} catch(e) {
+  console.log(e.name + ": " + e.msg + " at position " + e.position);
+}
+try {
+  p7 = new Parser("4.5 + (1.2 - 4)18e9");
+  console.log('Trying to parse "4.5 + (1.2 - 4)18e9"');
+  p7.parse();
+} catch(e) {
+  console.log(e.name + ": " + e.msg + " at position " + e.position);
+}
+try {
+  p8 = new Parser("4.5 + / 12.6");
+  console.log('Trying to parse "4.5 + / 12.6"');
+  p8.parse();
+} catch(e) {
+  console.log(e.name + ": " + e.msg + " at position " + e.position);
+}
+try {
+  p9 = new Parser("4.5 6.23e-2 + 9");
+  console.log('Trying to parse "4.5 6.23e-2 + 9"');
+  p9.parse();
+} catch(e) {
+  console.log(e.name + ": " + e.msg + " at position " + e.position);
+}
--- hw6/test/tokenizer_unit_tests.js
+++ hw6/test/tokenizer_unit_tests.js
@@ -5,11 +5,12 @@ assert = require('assert');
 t = new Tokenizer();
 test_string = "-12. + 0.5 * (-1   + 2 / 3.0e+2 / -.9)"
 
-t.add(/\d*.\d*(e|E)((-|\+)?)\d+/) // Parse scientific notation
- .add(/\d*\.\d*/)                 // Parse  floating points
- .add(/\d+/)                      // Parse integers
- .add(/\+|-|\*|\/|%|\(|\)/)       // Parse arithmetic operators and parenthesis
- .add(/\s*/)                      // Parse whitespaces
+t.add(/\d*\.?\d*(e|E)(-|\+)?\d+/) // Parse scientific notation
+ .add(/\d+\.\d*/)                // Parse floating points
+ .add(/\d*\.\d+/)                // Parse floating points
+ .add(/\d+/)                     // Parse integers
+ .add(/\+|-|\*|\/|%|\(|\)/)      // Parse arithmetic operators and parenthesis
+ .add(/\s+/)                     // Parse whitespaces
  .tokenize(test_string);
 
 // Test 1: Test current() function
@@ -29,13 +30,19 @@ assert(true == t.eof());
 
 // Test 4: Test TokenizerException() function
 try {
-  t.eat()
+  t.eat();
 } catch (e) {
   console.log(e.name + ": " + e.msg + " at position " + e.position);
 }
 try {
-  test_string = "-12.e + 0.5";
-  t.tokenize(test_string);
+  console.log('Trying to tokenize "-12.e + 0.5"')
+  t.tokenize("-12.e + 0.5");
 } catch (e) {
+  console.log(e.name + ": " + e.msg + " at position " + e.position);
+}
+try {
+  console.log('Trying to tokenize "13.4 . / 2.3"')
+  t.tokenize("13.4 . / 2.3");
+} catch(e) {
   console.log(e.name + ": " + e.msg + " at position " + e.position);
 }

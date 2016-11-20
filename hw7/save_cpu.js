c1e04a3e7cab26dfb44214b97c33a0929295a44c
Alex Chen <alexac9@u.washington.edu>
2016-11-20 12:21:10 -0800

    Add hw7/save_cpu.js

---
+++ hw7/save_cpu.js
@@ -0,0 +1,42 @@
+let jnet = require('./json_net');
+
+let client = jnet.JSONSocket();
+
+var test_com = function(port, host) {
+
+  let status = null;
+  let i = 0;
+  let input = [{ "command": "put", "key": "key1", "value": 14, "timestamp": 0 },
+    { "command": "put", "key": "key2", "value": 15, "timestamp": 0 }];
+
+  client.on('connect', function() { status = true; console.log("Connected to server."); });
+  client.setTimeout(5000);// If no response, assume port is not listening
+  client.on('timeout', function() { status = 'closed'; client.destroy(); });
+  client.on('error', function(exception) { status = 'closed'; });
+  client.on('close', function(exception) { console.log("Connection closed."); });
+  client.connect(port, host);
+
+  setTimeout(function() {
+    if(status) {
+      client.jwrite({"command":"ee590"},inputVals);
+    };
+  }, 1000);
+  function inputVals() {
+    setTimeout(function() {
+      let UNIXTIME = Math.floor(new Date() / 1000);
+      if(i < input.length) {
+        input[i].timestamp = UNIXTIME;
+        client.jwrite(input[i++], inputVals);
+        console.log(i);
+      } else {
+        console.log("Finished testing.");
+        client.end();
+      }
+    }, 1000);
+  };
+};
+
+var LAN = '10.0.0.193';   //Address to connect
+var PORT = 8080;         //data_server.js port
+
+test_com(PORT, LAN);

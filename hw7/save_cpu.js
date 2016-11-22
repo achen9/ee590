5666f3ae9f63eabd202e529997a689f159b3400b
Alex Chen <alexac9@u.washington.edu>
2016-11-21 22:03:59 -0800

    Update hw7/save_cpu.js to only execute next command after response from server

--- hw7/save_cpu.js
+++ hw7/save_cpu.js
@@ -1,6 +1,13 @@
 let jnet = require('./json_net');
 
 let client = jnet.JSONSocket();
+let input_index = 0;
+let commands = [
+    { "command": "put", "key": "cpu load", "value": 0, "timestamp": 0 },
+    { "command": "put", "key": "name", "value": "Alex Chen", "timestamp": 0 },
+    { "command": "get" },
+    { "command": "get", "key": "cpu load" },
+    { "command": "get", "key": "cpu load", "host": client.address().address }];
 
 var save_cpu_data = function(port, host) {
   let status = false;
@@ -8,13 +15,6 @@ var save_cpu_data = function(port, host) {
   let user_cpu_time = 0;
   let cpu_load = 0;
   let cpu_data = require('os').cpus();
-  let stored_data = { "command": "put", "key": "cpu load", "value": 0, "timestamp": 0 };
-  let stored_name = { "command": "put", "key": "name", "value": "Alex Chen", "timestamp": 0 };
-  let input_index = 0;
-  let get_inputs = [
-    { "command": "get" },
-    { "command": "get", "key": "cpu load" },
-    { "command": "get", "key": "cpu load", "host": client.address().address }];
   for(let i = 0; i < cpu_data.length; i++) {
     total_cpu_time = total_cpu_time +
       cpu_data[i].times.user +
@@ -26,7 +26,7 @@ var save_cpu_data = function(port, host) {
   };
   if(0 != total_cpu_time) {
     cpu_load = user_cpu_time / total_cpu_time;
-    stored_data.value = cpu_load;
+    commands[0].value = cpu_load;
   } else {
     console.error("total_cpu_time is zero.");
   }
@@ -39,36 +39,25 @@ var save_cpu_data = function(port, host) {
 
   setTimeout(function() {
     if(true == status) {
-      client.jwrite({"command":"ee590"},store_cpu_load);
+      client.jwrite({"command":"ee590"});
     };
   }, 1000);
-  function store_cpu_load() {
-    setTimeout(function() {
-      let UNIXTIME = Math.floor(new Date() / 1000);
-      stored_data.timestamp = UNIXTIME;
-      client.jwrite(stored_data, store_name);
-    }, 1000);
-  };
-  function store_name() {
-    setTimeout(function() {
-      let UNIXTIME = Math.floor(new Date() / 1000);
-      stored_name.timestamp = UNIXTIME;
-      client.jwrite(stored_name, get_cpu_load);
-    }, 1000);
-  };
-  function get_cpu_load() {
-    setTimeout(function() {
-      if(input_index < get_inputs.length) {
-        client.jwrite(get_inputs[input_index++], get_cpu_load);
-      } else {
-        console.log("Finished testing all get functions.");
-        client.end();
-      }
-    }, 1000);
-  }
 };
 client.on('json', function(object) {
   console.log(object);
+  if(2 > input_index) {
+    let UNIXTIME = Math.floor(new Date() / 1000);
+    commands[input_index].timestamp = UNIXTIME;
+    client.jwrite(commands[input_index++]);
+  } else if(input_index < commands.length) {
+    if(2 == input_index) {
+      console.log("Beginning get() function tests...");
+    }
+    client.jwrite(commands[input_index++]);
+  } else {
+    console.log("Finished testing all get() functions.");
+    client.end();
+  }
 });
 
 var LAN = process.argv[2];  //Address to connect

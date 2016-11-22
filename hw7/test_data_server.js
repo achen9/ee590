b063844773bdece23d1d4c6e0be10243d4b74420
Alex Chen <alexac9@u.washington.edu>
2016-11-21 22:02:43 -0800

    Update hw7/test_data_server.js to execute next command only after response from server

--- hw7/test_data_server.js
+++ hw7/test_data_server.js
@@ -1,24 +1,26 @@
 let jnet = require('./json_net');
 
 let client = jnet.JSONSocket();
+let i = 0;
+let input = [
+  { "command": "put" },
+  { "command": "put", "key": "key1" },
+  { "command": "put", "key": false, "value": 0.5 },
+  { "command": "put", "key": false, "value": 0.5, "timestamp": 45 },
+  { "command": "put", "key": "key1", "value": [1, 2, 3], "timestamp": 45 },
+  { "command": "put", "key": "key1", "value": 0.5, "timestamp": false },
+  { "command": "put", "key": "key1", "value": 0.5, "timestamp": -25 },
+  { "command": "put", "key": "key1", "value": 14, "timestamp": 0 },
+  { "command": "put", "key": "key2", "value": 15, "timestamp": 0 },
+  { "command": "get" },
+  { "command": "get", "key": "badkey" },
+  { "command": "get", "key": "key1", "host": "badhost" },
+  { "command": "get", "key": "key1" },
+  { "command": "get", "key": "key1", "host": "::ffff:10.0.0.193" }];
 
 var test_com = function(port, host) {
 
   let status = false;
-  let i = 0;
-  let input = [
-    { "command": "put" },
-    { "command": "put", "key": "key1" },
-    { "command": "put", "key": false, "value": 0.5, "timestamp": 0 },
-    { "command": "put", "key": "key1", "value": [1, 2, 3], "timestamp": 0 },
-    { "command": "put", "key": "key1", "value": 14, "timestamp": 0 },
-    { "command": "put", "key": "key2", "value": 15, "timestamp": 0 },
-    { "command": "get" },
-    { "command": "get", "key": "badkey" },
-    { "command": "get", "key": "key1", "host": "badhost" },
-    { "command": "get", "key": "key1" },
-    { "command": "get", "key": "key1", "host": "::ffff:10.0.0.193" }];
-
   client.on('connect', function() { status = true; console.log("Connected to server."); });
   client.setTimeout(5000);// If no response, assume port is not listening
   client.on('timeout', function() { status = 'closed'; client.destroy(); });
@@ -28,24 +30,22 @@ var test_com = function(port, host) {
 
   setTimeout(function() {
     if(true == status) {
-      client.jwrite({"command":"ee590"},inputVals);
+      client.jwrite({"command":"ee590"});
     };
   }, 1000);
-  function inputVals() {
-    setTimeout(function() {
-      let UNIXTIME = Math.floor(new Date() / 1000);
-      if(i < input.length) {
-        input[i].timestamp = UNIXTIME;
-        client.jwrite(input[i++], inputVals);
-      } else {
-        console.log("Finished testing.");
-        client.end();
-      }
-    }, 1000);
-  };
 };
 client.on('json', function(object) {
   console.log(object);
+  let UNIXTIME = Math.floor(new Date() / 1000);
+  if(9 > i && 6 < i) {
+    input[i].timestamp = UNIXTIME;
+    client.jwrite(input[i++]);
+  } else if (i >= input.length) {
+    console.log("Finished testing.");
+    client.end();
+  } else {
+    client.jwrite(input[i++]);
+  }
 });
 
 var LAN = process.argv[2]; //Address to connect

ae77a8f7492d5018f65d65cf0b207176d00b5154
Alex Chen <alexac9@u.washington.edu>
2016-12-04 14:10:41 -0800

    Add hw8/prob6.sql

---
+++ hw8/prob6.sql
@@ -0,0 +1,12 @@
+.headers on
+SELECT Title,
+       NumSongs,
+       Duration
+FROM Album
+JOIN
+  (SELECT AlbumId, 
+         strftime("%H:%M:%S", datetime(Round(sum(Milliseconds)/1000.0),"unixepoch")) AS Duration, 
+         count(Milliseconds) AS NumSongs
+  FROM Track
+  GROUP BY AlbumId)
+USING (AlbumId);

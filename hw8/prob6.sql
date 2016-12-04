3ed4da07994f998aeefdf92056a4a3d46aa4ff54
Alex Chen <alexac9@u.washington.edu>
2016-12-04 14:28:43 -0800

    Reformat hw8/prob6.sql

--- hw8/prob6.sql
+++ hw8/prob6.sql
@@ -5,8 +5,8 @@ SELECT Title,
 FROM Album
 JOIN
   (SELECT AlbumId, 
-         strftime("%H:%M:%S", datetime(Round(sum(Milliseconds)/1000.0),"unixepoch")) AS Duration, 
-         count(Milliseconds) AS NumSongs
-  FROM Track
-  GROUP BY AlbumId)
+          strftime("%H:%M:%S", datetime(Round(sum(Milliseconds)/1000.0),"unixepoch")) AS Duration, 
+          count(Milliseconds) AS NumSongs
+   FROM Track
+   GROUP BY AlbumId)
 USING (AlbumId);

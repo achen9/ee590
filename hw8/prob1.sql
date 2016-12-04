f44f60cdd570cd85a7ca880bebaa19dc0a20d350
Alex Chen <alexac9@u.washington.edu>
2016-12-04 14:10:26 -0800

    Reformat hw8/prob1.sql

--- hw8/prob1.sql
+++ hw8/prob1.sql
@@ -1,5 +1,7 @@
 .headers on
-SELECT Title, Name, strftime("%M:%S", datetime(Round(Milliseconds/1000.0),"unixepoch")) AS Duration
+SELECT Title, 
+       Name, 
+       strftime("%M:%S", datetime(Round(Milliseconds/1000.0),"unixepoch")) AS Duration
 FROM Album 
 JOIN Track USING (AlbumId) 
 WHERE AlbumId=

0c82afb6f11407c5db17db342b120e64227bfd92
Alex Chen <alexac9@u.washington.edu>
2016-12-04 13:52:03 -0800

    Add hw8/prob4.sql

---
+++ hw8/prob4.sql
@@ -0,0 +1,10 @@
+.headers on
+SELECT Name AS Track, Quantity
+FROM Track 
+JOIN
+  (SELECT TrackId, sum(Quantity) AS Quantity 
+   FROM InvoiceLine
+   GROUP BY TrackId)
+USING (TrackId)
+ORDER BY Quantity desc
+LIMIT 10;

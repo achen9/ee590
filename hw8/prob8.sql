5105214e6d66bb422f63e2b677eaed3b7b610c4a
Alex Chen <alexac9@u.washington.edu>
2016-12-04 17:08:53 -0800

    Add hw8/prob8.sql

---
+++ hw8/prob8.sql
@@ -0,0 +1,20 @@
+.headers on
+SELECT Name,
+       Artist_Revenue
+FROM Artist
+JOIN
+  (SELECT ArtistId,
+          sum(AlbumRevenue) AS Artist_Revenue
+   FROM Album
+   JOIN
+     (SELECT AlbumId,
+             sum(UnitPrice*Quantity) AS AlbumRevenue
+      FROM InvoiceLine
+      JOIN Track
+      USING (TrackId, UnitPrice)
+      GROUP BY AlbumId)
+   USING (AlbumId)
+   GROUP BY ArtistId)
+USING (ArtistId)
+ORDER BY Artist_Revenue desc
+LIMIT 10;
